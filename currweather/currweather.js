39582db0fe6ac3c604a1c95f1edf36471cbea021
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-01 14:26:18 -0600

    currweather now outputs current weather or any city based on lat and long coords.

--- currweather/currweather.js
+++ currweather/currweather.js
@@ -1,6 +1,13 @@
-function getWeather()
-{
-    lat = document.getElementById("latitude").value
-    long = document.getElementById("longitude").value
-    console.log(fetch('https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m').then(response => console.log(response.text())))
+// gets current weather of passed in city coordinates
+function getWeather() {
+    const lat = document.getElementById("latitude").value
+    const long = document.getElementById("longitude").value
+    const weatherObj = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${parseFloat(lat)}&longitude=${parseFloat(long)}&current_weather=true`);
+    weatherObj.then((response) => {
+        const weatherJson = response.json();
+        weatherJson.then((data) => {
+            document.getElementById("currWeather").value = data.current_weather['temperature']
+        });
+    });
+
 } 
