b66e0dba21833fe865c7dacc142af28344ec0f1a
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-01 10:56:53 -0600

    push everything

--- currweather/currweather.js
+++ currweather/currweather.js
@@ -1,5 +1,5 @@
 function getWeather()
 {
     city = document.getElementById("city").value
-    console.log(fetch("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={c1b8a9a5529c12d8bafe0f77f130217e}").then(response => response.text()))
+    console.log(fetch("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={c1b8a9a5529c12d8bafe0f77f130217e}").then(response => console.log(response.text())))
 }
