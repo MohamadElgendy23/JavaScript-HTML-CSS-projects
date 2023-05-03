75e77c80f2f01495c80bf8e3a11dfb6d972d5fbe
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-03 02:48:02 -0600

    edited currweather.js to make it look more stylish and have logic for coordinates entered

--- currweather/currweather.js
+++ currweather/currweather.js
@@ -1,13 +1,21 @@
 // gets current weather of passed in city coordinates
 function getWeather() {
     const lat = document.getElementById("latitude").value
+    if (lat < -90 || lat > 90) {
+        window.alert("Latitude must be in range of -90 to 90")
+        return
+    }
     const long = document.getElementById("longitude").value
+    if (long < -180 || long > 180) {
+        window.alert("Longitude must be in range of -180 to 180")
+        return
+    }
     const weatherObj = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${parseFloat(lat)}&longitude=${parseFloat(long)}&current_weather=true`);
     weatherObj.then((response) => {
         const weatherJson = response.json();
         console.log(weatherJson)
         weatherJson.then((weatherData) => {
-            document.getElementById("currWeather").value = weatherData.current_weather['temperature']
+            document.getElementById("temperature").value = weatherData.current_weather['temperature']
         });
     });
 
