068cfb96a17b757c30e52d12ba702edd84cf583d
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-06 22:12:41 -0600

    can now click buttons

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,4 +1,27 @@
-function getSelection()
-{
-    
+function getSelection1() {
+    document.getElementById("button1").innerHTML = 'X';
+}
+function getSelection2() {
+    document.getElementById("button2").innerHTML = 'X';
+}
+function getSelection3() {
+    document.getElementById("button3").innerHTML = 'X';
+}
+function getSelection4() {
+    document.getElementById("button4").innerHTML = 'X';
+}
+function getSelection5() {
+    document.getElementById("button5").innerHTML = 'X';
+}
+function getSelection6() {
+    document.getElementById("button6").innerHTML = 'X';
+}
+function getSelection7() {
+    document.getElementById("button7").innerHTML = 'X';
+}
+function getSelection8() {
+    document.getElementById("button8").innerHTML = 'X';
+}
+function getSelection9() {
+    document.getElementById("button9").innerHTML = 'X';
 }
