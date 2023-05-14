6a176211653f40769f9a3431d4fcf0d2dc6976c7
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-14 15:49:26 -0600

    tic tac toe game works

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,19 +1,238 @@
-//start
-let p1 = 1;
-let win = false;
+//player starts with 'X'
+let p = 'X';
+document.getElementById("playagain").hidden = true;
 
 function displayInput(square) {
-    if (p1 == 1)
-    {
+    if (p == 'X') {
         document.getElementById(square).innerHTML = 'X';
-        p1=0;
+        if (isXWin(square)) {
+            window.alert("X won!");
+            document.getElementById("playagain").innerHTML = "Play Again!";
+            document.getElementById("playagain").hidden = false;
+        }
+        else
+        {
+            p = 'O';
+        }
     }
-    else
-    {
+    else {
         document.getElementById(square).innerHTML = 'O';
-        p1=1;
+        if (isOWin(square)) {
+            window.alert("O won!");
+            document.getElementById("playagain").innerHTML = "Play Again!";
+            document.getElementById("playagain").hidden = false;
+        }
+        else
+        {
+            p = 'X';
+        }
     }
 }
+//for each square, check win logic for X
+function isXWin(square) {
+    if (square == "button1") {
+        if (document.getElementById("button2").innerHTML == 'X' && document.getElementById("button3").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'X' && document.getElementById("button9").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button4").innerHTML == 'X' && document.getElementById("button7").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button2") {
+        if (document.getElementById("button1").innerHTML == 'X' && document.getElementById("button3").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'X' && document.getElementById("button8").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button3") {
+        if (document.getElementById("button1").innerHTML == 'X' && document.getElementById("button2").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button6").innerHTML == 'X' && document.getElementById("button9").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'X' && document.getElementById("button7").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button4") {
+        if (document.getElementById("button1").innerHTML == 'X' && document.getElementById("button7").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'X' && document.getElementById("button6").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button5") {
+        if (document.getElementById("button1").innerHTML == 'X' && document.getElementById("button9").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button3").innerHTML == 'X' && document.getElementById("button7").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button4").innerHTML == 'X' && document.getElementById("button6").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button2").innerHTML == 'X' && document.getElementById("button8").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button6") {
+        if (document.getElementById("button3").innerHTML == 'X' && document.getElementById("button9").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button4").innerHTML == 'X' && document.getElementById("button5").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button7") {
+        if (document.getElementById("button1").innerHTML == 'X' && document.getElementById("button4").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button8").innerHTML == 'X' && document.getElementById("button9").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button3").innerHTML == 'X' && document.getElementById("button5").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button8") {
+        if (document.getElementById("button7").innerHTML == 'X' && document.getElementById("button9").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button2").innerHTML == 'X' && document.getElementById("button5").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+    else {
+        if (document.getElementById("button1").innerHTML == 'X' && document.getElementById("button5").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button3").innerHTML == 'X' && document.getElementById("button6").innerHTML == 'X') {
+            return true;
+        }
+        if (document.getElementById("button7").innerHTML == 'X' && document.getElementById("button8").innerHTML == 'X') {
+            return true;
+        }
+        return false;
+    }
+}
+//for each square, check win logic for O
+function isOWin(square) {
+    if (square == "button1") {
+        if (document.getElementById("button2").innerHTML == 'O' && document.getElementById("button3").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'O' && document.getElementById("button9").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button4").innerHTML == 'O' && document.getElementById("button7").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button2") {
+        if (document.getElementById("button1").innerHTML == 'O' && document.getElementById("button3").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'O' && document.getElementById("button8").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button3") {
+        if (document.getElementById("button1").innerHTML == 'O' && document.getElementById("button2").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button6").innerHTML == 'O' && document.getElementById("button9").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'O' && document.getElementById("button7").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button4") {
+        if (document.getElementById("button1").innerHTML == 'O' && document.getElementById("button7").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button5").innerHTML == 'O' && document.getElementById("button6").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button5") {
+        if (document.getElementById("button1").innerHTML == 'O' && document.getElementById("button9").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button3").innerHTML == 'O' && document.getElementById("button7").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button4").innerHTML == 'O' && document.getElementById("button6").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button2").innerHTML == 'O' && document.getElementById("button8").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button6") {
+        if (document.getElementById("button3").innerHTML == 'O' && document.getElementById("button9").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button4").innerHTML == 'O' && document.getElementById("button5").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button7") {
+        if (document.getElementById("button1").innerHTML == 'O' && document.getElementById("button4").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button8").innerHTML == 'O' && document.getElementById("button9").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button3").innerHTML == 'O' && document.getElementById("button5").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else if (square == "button8") {
+        if (document.getElementById("button7").innerHTML == 'O' && document.getElementById("button9").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button2").innerHTML == 'O' && document.getElementById("button5").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+    else {
+        if (document.getElementById("button1").innerHTML == 'O' && document.getElementById("button5").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button3").innerHTML == 'O' && document.getElementById("button6").innerHTML == 'O') {
+            return true;
+        }
+        if (document.getElementById("button7").innerHTML == 'O' && document.getElementById("button8").innerHTML == 'O') {
+            return true;
+        }
+        return false;
+    }
+}
+//each button/square selection
 function getSelection1() {
     displayInput("button1");
 }
@@ -40,4 +259,17 @@ function getSelection8() {
 }
 function getSelection9() {
     displayInput("button9");
+}
+function playAgain()
+{
+    //couldnt figure out how to change children just with the parent
+    document.getElementById("button1").innerHTML = "";
+    document.getElementById("button2").innerHTML = "";
+    document.getElementById("button3").innerHTML = "";
+    document.getElementById("button4").innerHTML = "";
+    document.getElementById("button5").innerHTML = "";
+    document.getElementById("button6").innerHTML = "";
+    document.getElementById("button7").innerHTML = "";
+    document.getElementById("button8").innerHTML = "";
+    document.getElementById("button9").innerHTML = "";
 }
