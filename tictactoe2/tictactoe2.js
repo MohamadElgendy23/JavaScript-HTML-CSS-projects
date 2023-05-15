c118da810fa8a7e29d141e298153427587815ec7
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-15 17:31:40 -0600

    shorter code and better logic for tic tac toe

---
+++ tictactoe2/tictactoe2.js
@@ -0,0 +1,63 @@
+let p1 = 1;
+startGame();
+
+// start the game
+function startGame() {
+    document.getElementById("playagain").hidden = true;
+    //set click for each button
+    const buttons = document.getElementById("grid-container").children;
+    for (button of buttons) {
+        button.onclick = clickGameButton;
+    }
+
+}
+//button click logic
+function clickGameButton(event) {
+    const button = event.target;
+    if (p1 == 1 && !button.innerHTML) {
+        button.innerHTML = 'X';
+        p1 = 0;
+    }
+    if (p1 != 1 && !button.innerHTML) {
+        button.innerHTML = 'O';
+        p1 = 1;
+    }
+    if (isWin('X'))
+    {
+        alert('X Wins!');
+    }
+    if (isDraw()) {
+        alert("Draw!");
+        document.getElementById("playagain").hidden = false;
+        document.getElementById("playagain").innerHTML = "Play Again!";
+    }
+}
+//play the game again
+function playAgain() {
+    p1 = 1;
+    const buttons = document.getElementById("grid-container").children;
+    for (const button of buttons) {
+        button.innerHTML = "";
+    }
+    document.getElementById("playagain").hidden = true;
+}
+//is it a win?
+function isWin(letter) {
+    const buttons = document.getElementById("grid-container").children;
+    
+    return false;
+
+}
+//is it a draw?
+function isDraw()
+{
+    const buttons = document.getElementById("grid-container").children;
+    for (const button of buttons)
+    {
+        if (button.innerHTML=="")
+        {
+            return false;
+        }
+    }
+    return true;
+}
