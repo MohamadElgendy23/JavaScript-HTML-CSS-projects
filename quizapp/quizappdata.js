659594b521c23b12ca833a02e274fa1f22d467f3
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-05-22 14:11:00 -0600

    currently working on a quiz app. 5 questions. going to be made with react as well.

--- quizapp/quizappdata.js
+++ quizapp/quizappdata.js
@@ -2,17 +2,39 @@
 
 export const quizappdata = [
     {
-        question: "What is the tallest building in the world?",
-        answerChoices: ['Eifel Tower', ''],
-        answer: ""
-
-
-
-
+        questionID: 1,
+        question: 'What is the tallest building in the world?',
+        answerChoices: ['The Eifel Tower', 'Empire State Building', 'Burj Khalifa', 'Statue of Liberty'],
+        answer: 'Burj Khalifa'
     },
 
+    {
+        questionID: 2,
+        question: 'What kind of data structure contains no duplicates?',
+        answerChoices: ['Stack', 'Set', 'Queue', 'List'],
+        answer: 'Set'
+    },
 
+    {
+        questionID: 3,
+        question: 'What kind of data structure contains no duplicates?',
+        answerChoices: ['Stack', 'Set', 'Queue', 'List'],
+        answer: 'Set'
+    },
 
+    {
+        questionID: 4,
+        question: 'What kind of data structure contains no duplicates?',
+        answerChoices: ['Stack', 'Set', 'Queue', 'List'],
+        answer: 'Set'
+    },
 
+    {
+        questionID: 5,
+        question: 'What kind of data structure contains no duplicates?',
+        answerChoices: ['Stack', 'Set', 'Queue', 'List'],
+        answer: 'Set'
+    },
 
 ];
+
