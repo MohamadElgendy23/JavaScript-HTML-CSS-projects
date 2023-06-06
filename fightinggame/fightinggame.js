824cdf96806b22a5bc6beccab3af81199eccbbe1
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-06-06 00:53:49 -0600

    now rectangles (sprites) change position using a requestAnimationFrame that changes position of x and y by 5 and 10 respectively.

--- fightinggame/fightinggame.js
+++ fightinggame/fightinggame.js
@@ -1,7 +1,8 @@
+const gameCanvas = document.querySelector("canvas"); //the game canvas
+const gameCanvasContext = gameCanvas.getContext("2d"); //the canvas context used to draw things on the canvas
+
 //creates the game canvas that will display the fighting game
 function createGameCanvas() {
-  const gameCanvas = document.querySelector("canvas"); //the game canvas
-  const gameContext = gameCanvas.getContext("2d"); //the canvas context
   function initialize() {
     // Register an event listener to call the resizeCanvas() function
     // each time the window is resized.
@@ -10,7 +11,7 @@ function createGameCanvas() {
     resizeCanvas();
   }
   // Runs each time the DOM window resize event fires.
-  // Resets the canvas dimensions to match window and redraws.
+  // Resets the canvas dimensions to match window dimensions and redraws.
   const resizeCanvas = () => {
     gameCanvas.width = window.innerWidth;
     gameCanvas.height = window.innerHeight;
@@ -19,21 +20,52 @@ function createGameCanvas() {
 
   // Display custom canvas
   const redrawCanvas = () => {
-    gameContext.fillStyle = "Black";
-    gameContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height); //start at top left of the canvas and fill up the whole canvas
+    gameCanvasContext.fillStyle = "Black";
+    gameCanvasContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height); //start at top left of the canvas and fill up the whole canvas
   };
 
   initialize();
 }
-createGameCanvas();
+createGameCanvas(); //create!
 
 //represents the movable items (characters) in the game => characters = player and enemy
 class Sprite {
-  constructor(position) {
-    this.positon = position;
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+
+  //draw the sprite at this.position on the canvas
+  draw() {
+    gameCanvasContext.fillStyle = "Red";
+    gameCanvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += 5;
+    this.position.y += 10;
   }
 }
 
-const player = new Sprite({ x: 0, y: 0 });
+const player = new Sprite({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
 
-console.log(player);
+const enemy = new Sprite({
+  position: { x: 600, y: 150 },
+  velocity: { x: 0, y: 0 },
+});
+
+//runs animation frame infinitely. used as update loop.
+function animate() {
+  window.requestAnimationFrame(animate);
+  gameCanvasContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  gameCanvasContext.fillStyle = "Black";
+  gameCanvasContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height); //start at top left of the canvas and fill up the whole canvas
+  player.update();
+  enemy.update();
+}
+
+animate();
