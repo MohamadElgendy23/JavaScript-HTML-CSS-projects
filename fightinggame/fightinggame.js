d189c3292391e6ba5793c8ab77b6f171e3a5e3ac
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-06-06 12:42:36 -0600

    changed the dimensions of the canvas so that it isnt the full window dimensions also creating the velocity and position logic so that if a sprite lands on the ground it doesnt keep falling and just stops (set velocity to 0)

--- fightinggame/fightinggame.js
+++ fightinggame/fightinggame.js
@@ -13,8 +13,8 @@ function createGameCanvas() {
   // Runs each time the DOM window resize event fires.
   // Resets the canvas dimensions to match window dimensions and redraws.
   const resizeCanvas = () => {
-    gameCanvas.width = window.innerWidth;
-    gameCanvas.height = window.innerHeight;
+    gameCanvas.width = 1024; //not using css here because with canvas dimensions it wasnt correct
+    gameCanvas.height = 576;
     redrawCanvas();
   };
 
@@ -30,40 +30,53 @@ createGameCanvas(); //create!
 
 //represents the movable items (characters) in the game => characters = player and enemy
 class Sprite {
-  constructor({ position, velocity }) {
+  constructor({ position, velocity, width, height }) {
     this.position = position;
     this.velocity = velocity;
+    this.width = width;
+    this.height = height;
   }
 
   //draw the sprite at this.position on the canvas
   draw() {
     gameCanvasContext.fillStyle = "Red";
-    gameCanvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+    gameCanvasContext.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
   }
 
   update() {
     this.draw();
-    this.position.x += 5;
-    this.position.y += 10;
+    this.position.y += this.velocity.y; //move downward at specifc velocity
+    //let sprite stop when it hits floor
+    if (this.position.y + this.height === 824) {
+      this.velocity.y = 0;
+    }
   }
 }
 
 const player = new Sprite({
   position: { x: 0, y: 0 },
-  velocity: { x: 0, y: 0 },
+  velocity: { x: 0, y: 5 },
+  width: 100,
+  height: 150,
 });
 
 const enemy = new Sprite({
   position: { x: 600, y: 150 },
-  velocity: { x: 0, y: 0 },
+  velocity: { x: 0, y: 5 },
+  width: 100,
+  height: 400,
 });
 
 //runs animation frame infinitely. used as update loop.
 function animate() {
   window.requestAnimationFrame(animate);
-  gameCanvasContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
   gameCanvasContext.fillStyle = "Black";
-  gameCanvasContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height); //start at top left of the canvas and fill up the whole canvas
+  gameCanvasContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height); //differentiate between red sprite and black background
   player.update();
   enemy.update();
 }
