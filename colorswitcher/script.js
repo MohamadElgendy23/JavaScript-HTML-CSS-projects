8b069a26599dedd953b44b7189134a14401f08b3
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-19 20:45:06 -0600

    added anchor for the main header

--- colorswitcher/script.js
+++ colorswitcher/script.js
@@ -10,6 +10,9 @@ const foregroundColorInputContainer = document.querySelector(
   ".fg-color-container"
 );
 
+// current option mode (Name is default)
+let currOption = "Name";
+
 // make the dropdown options "toggle" when user clicks the button
 dropDownButton.addEventListener("click", () => {
   dropDownOptions.style.display === "flex"
@@ -20,12 +23,27 @@ dropDownButton.addEventListener("click", () => {
 // when user clicks one of the options, change the html appropriately
 dropDownOptions.addEventListener("click", (event) => {
   // "Name" option
-  if (event.target.innerText === "Name") {
+  if (event.target.innerText === "Name" && currOption !== "Name") {
+    currOption = "Name";
+    backgroundColorInputContainer.children[0].innerText =
+      "What Background Color? (Name)";
+    foregroundColorInputContainer.children[0].innerText =
+      "What Foreground Color? (Name)";
   }
   // "Hex" option
-  else if (event.target.innerText === "Hex") {
+  if (event.target.innerText === "Hex" && currOption !== "Hex") {
+    currOption = "Hex";
+    backgroundColorInputContainer.children[0].innerText =
+      "What Background Color? (Hex)";
+    foregroundColorInputContainer.children[0].innerText =
+      "What Foreground Color? (Hex)";
   }
   // "Palette" option
-  else {
+  if (event.target.innerText === "Palette" && currOption !== "Palette") {
+    currOption = "Palette";
+    backgroundColorInputContainer.children[0].innerText =
+      "Select A Background Color";
+    foregroundColorInputContainer.children[0].innerText =
+      "Select A Foreground Color";
   }
 });
