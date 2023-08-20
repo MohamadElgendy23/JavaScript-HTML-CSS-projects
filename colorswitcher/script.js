c9b82c30f0c0dd6c3f4d7696b00e59fb32832fe0
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-19 19:55:28 -0600

    adding js logic to toggle options button

--- colorswitcher/script.js
+++ colorswitcher/script.js
@@ -1,2 +1,10 @@
-// make the dropdown options button "toggle" when user clicks it
-document.ge
+// useful variables
+const dropDownButton = document.getElementById("dropdown-button");
+const dropDownOptions = document.getElementById("dropdown-options");
+
+// make the dropdown options "toggle" when user clicks the button
+dropDownButton.addEventListener("click", () => {
+  dropDownOptions.style.display === "none"
+    ? (dropDownOptions.style.display = "flex")
+    : (dropDownOptions.style.display = "none");
+});
