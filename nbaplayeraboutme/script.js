6813c235dfa5978ff45ca2adf0ca4003544db1aa
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-21 15:02:12 -0600

    time to implement the javascript logic, first things first; get the html elements needed.

--- nbaplayeraboutme/script.js
+++ nbaplayeraboutme/script.js
@@ -1,3 +1,5 @@
-const playersArr = [{
-    
-}, {}, {}];
+// the 3 players array. contains the info and image to be displayed on selection.
+const playersArr = [{}, {}, {}];
+
+//useful variables
+const selectPlayerContainer = 
