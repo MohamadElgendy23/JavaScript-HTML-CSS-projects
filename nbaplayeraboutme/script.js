53e6a6c6c4ea91874e9f449ae9a940d346a0c4b9
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-21 19:20:36 -0600

    implemented the playersArr with all the player info that will be displayed

--- nbaplayeraboutme/script.js
+++ nbaplayeraboutme/script.js
@@ -1,22 +1,42 @@
 // the 3 players array. contains the info and image to be displayed on selection.
 const playersArr = [
+  // Lebron James
   {
     image:
       "https://nbc-2.com/wp-content/uploads/2020/07/hypat…1a8d275bcb-h_27f46f360593d4f3b292f98dc5e1db17.jpg",
     whoami:
       "I am Lebron James, an NBA champion who currently plays for the Los Angeles Lakers. I am also referred to as King James or the GOAT since I am arguably the best basketball player in the world.",
+    careertimeline: [
+      "Named Ohio's Mr. Basketball three times while leading his highschool",
+      "Earned three Ohio State championships in college and then attended the NBA draft at only 18 years old",
+      "After playing for multiple years and dominating the competition, now perceived as the best NBA player in history",
+    ],
   },
+
+  // Stephen Curry
   {
     image:
       "https://blacksportsonline.com/wp-content/uploads/2023/08/Kobe-Bryant-angry.jpg",
     whoami:
       "I am Stephen Curry, the 3 point master who before shooting a 3, better pray the ball is flat. I currently play for the Golden State Warriors and enjoy holding training camps to help inspire the future generation.",
+    careertimeline: [
+      "Led all first-year players in the country as a college freshman",
+      "Led 10th-seeded Davidson on an improbable run to the Elite Eight as a sophomore",
+      "Entered the 2009 NBA draft and got selected by the Warriors. After that it was history",
+    ],
   },
+
+  // Damian Lillard
   {
     image:
       "https://nypost.com/wp-content/uploads/sites/2/2020/07/Damian-Lillard.jpg?quality=90&strip=all",
     whoami:
       "I am Damian Lillard, aka Dame D.O.L.L.A. I play for the Portland Trailblazers where I stay loyal to my team despite losses and doubts by the fans.",
+    careertimeline: [
+      "As a freshman at Weber State, averaged 11.5 points per game and was named the Big Sky Conference Freshman of the Year",
+      "Led the Wildcats to the conference championship sophomore year",
+      "As a top guard in the country, entered the NBA draft in 2012 and dominated the comp as a Trailblazer",
+    ],
   },
 ];
 
